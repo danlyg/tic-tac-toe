2f54fd965675262999c4a8ee13913c2adf837623
Daniel Gih <dgih@Daniels-MacBook-Pro-2.local>
2015-05-03 17:48:35 -0700

    fixed checkwinner

--- ticTacToe.js
+++ ticTacToe.js
@@ -2,67 +2,86 @@
 window.addEventListener('load', function (event) {
   // all your code should go in here.
 
-function clearBoard(){
-	//clears the board
-}
-
-function alternate(){
-	if(current === 'X'){
-		newCurrent = 'O';
-		return newCurrent;
-	} else {
-		newCurrent = 'X';
-		return newCurrent;
+	function clearBoard(){
+		//clears the board
 	}
-}
 
-function checkwinner(){
-	//if 012 || 345 || 678...
-	var winningcombs = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
-	for (var i = winningcombs.length - 1; i >= 0; i--) {
-		if (boxarr[winningcombs[i][0]] = boxarr[winningcombs[i][1]] = boxarr[winningcombs[i][2]]){
-			return true;
-		} else{
-			return false;
+	function alternate(){
+		if(current === 'X'){
+			newCurrent = 'O';
+			return newCurrent;
+		} else {
+			newCurrent = 'X';
+			return newCurrent;
+		}
+	}
+
+	function checkWinner(){
+		//if 012 || 345 || 678...
+		var winningcombs = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]];
+		
+
+		// console.log('entering loop for checkwinner')
+		for (var i = 0; i <winningcombs.length; i++) {
+			
+			console.log('inside loop checking for a winner')
+			var a = boxarr[winningcombs[i][0]].innerHTML;
+			var b = boxarr[winningcombs[i][1]].innerHTML;
+			var c = boxarr[winningcombs[i][2]].innerHTML;
+
+			// console.log('checking if A === B ', a === b)
+			// console.log('checking if B === C ', b === c)
+			// console.log('value of b', b.trim(), 'type of b', typeof(b), b.length)
+			// console.log('checking if b!==&nbsp;', b !=='&nbsp;')
+
+
+			if ((a===b) && (b===c) && (b !== '&nbsp;')){
+				// console.log('found a winner')
+				return true;
+			}
+			// console.log('no winner yet checking next possible combination')
+		};
+		// console.log('no winner found')
+		return false;
+	}
+
+	var boxarr = [document.querySelector('#box1'),
+	document.querySelector('#box2'),
+	document.querySelector('#box3'),
+	document.querySelector('#box4'),
+	document.querySelector('#box5'),
+	document.querySelector('#box6'),
+	document.querySelector('#box7'),
+	document.querySelector('#box8'),
+	document.querySelector('#box9')]
+
+
+	var current = 'X'
+
+	var spaceoccupied = [false, false, false, false, false, false, false, false, false]
+
+
+
+
+	for (var i = 0; i < 9; i++) {
+		if (spaceoccupied[i] === false){ //notworking
+			console.log(spaceoccupied[i], 'is this space ' + i + ' occupied?')
+
+
+			boxarr[i].addEventListener('click', function(event){
+				this.innerHTML = current;
+				current = alternate(current);
+				spaceoccupied[i] = true; //notupdating?
+
+
+				winner = checkWinner();
+					if(winner){
+						alert(current +' loses');
+					}
+
+			});
 		}
 	};
-}
-
-var boxarr = [document.querySelector('#box1'),
-document.querySelector('#box2'),
-document.querySelector('#box3'),
-document.querySelector('#box4'),
-document.querySelector('#box5'),
-document.querySelector('#box6'),
-document.querySelector('#box7'),
-document.querySelector('#box8'),
-document.querySelector('#box9')]
-
-
-var current = 'X'
-
-var spaceoccupied = [false, false, false, false, false, false, false, false, false]
-
-
-
-
-for (var i = 0; i < 9; i++) {
-	if (spaceoccupied[i] === false){ //notworking
-
-		boxarr[i].addEventListener('click', function(event){
-			this.innerHTML = current;
-			current = alternate(current);
-			spaceoccupied[i] = true; //notupdating?
-
-
-			winner = checkforwinner();
-				if(winner){
-					alert(current +'loses');
-				}
-
-		});
-	}
-};
 
 
 
